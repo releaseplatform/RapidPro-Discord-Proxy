4c4f65352d06225e28233aa28a3c875f3802a658
Awen Saunders <me@awen.io>
2020-07-27 11:39:16 -0700

    Parameterise messaging
    Now we can send to specific users and channels if we know their IDs.
    There are two endpoints for this. Still need to figure out if we need to
    support multiple bot users (probably), and deal with incoming messages
    in a more elegant way than just printing them. If we need to have more
    than just one bot user, we'll also need some kind of
    authentication/identification of which bot to send from.

--- rapidprodiscordproxy/discord_handler.py
+++ rapidprodiscordproxy/discord_handler.py
@@ -11,13 +11,21 @@ class DiscordHandler(discord.Client):
     async def on_message(self, message: discord.Message):
         if self.user == message.author:
             return  # We don't want to handle messages we've sent.
-        print(f"Message received from {message.author}: {message.content}")
+        print(
+            f"Message received from: {message.author}\n"
+            f"guild: {message.guild}\n"
+            f"channel: {message.channel}\n"
+            f"content: {message.content}\n"
+        )
 
-    async def send_to_awen(self, message: str):
+    async def send_dm(self, message: str, user_id: int):
         # channel: discord.TextChannel = self.get_channel(737097898738581599)
         # await channel.send(message)
-        user: discord.User = self.get_user(694634743521607802)
+        user: discord.User = self.get_user(user_id)
         if user.dm_channel is None:
             await user.create_dm()
         await user.dm_channel.send(message)
 
+    async def send_channel(self, message: str, channel_id: int):
+        channel: discord.GroupChannel = self.get_channel(channel_id)
+        await channel.send(message)
--- rapidprodiscordproxy/main.py
+++ rapidprodiscordproxy/main.py
@@ -7,16 +7,34 @@ app = FastAPI()
 
 client: DiscordHandler
 
+token: str
+
+with open("./discord-creds-DO-NOT-COMMIT.txt", "r") as f:
+    token = f.read().strip()
+
+print(token)
+
 
 @app.get("/")
 async def root():
     return {"message": "hello"}
 
 
-@app.post("/sendtoawen")
-async def send_to_awen(message: str):
-    print(message)
-    await client.send_to_awen(message)
+@app.post("/discord/direct/send/")
+async def send_discord_DM(message: str, recipient_id: int):
+    """
+    Sends a message to a specified user in their DMs.
+    Must have the permissions to send messages.
+    recipient_id is the internal ID for the discord user.
+    It can be found using the discord client with developer mode enabled,
+    right clicking on the user
+    """
+    await client.send_dm(message, recipient_id)
+
+
+@app.post("/discord/channel/send/")
+async def send_discord_channel(message: str, channel_id: int):
+    await client.send_channel(message, channel_id)
 
 
 @app.on_event("startup")
@@ -24,7 +42,7 @@ async def startup():
     global client
     client = DiscordHandler(loop=asyncio.get_running_loop())
     print("logging in")
-    await client.login("<SECRET GOES HERE>"),
+    await client.login(token),
     asyncio.create_task(
         client.connect(), name="Discord Task",
     )
